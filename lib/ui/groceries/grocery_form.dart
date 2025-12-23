3f4bbc8bd50ab0e832bbd947f3a58516eab203c8
satpanha <panha.sat618@gmail.com>
2025-12-23 14:09:42 +0700

    first commit

---
+++ lib/ui/groceries/grocery_form.dart
@@ -0,0 +1,164 @@
+import 'dart:js_interop';
+
+import 'package:flutter/material.dart';
+import 'package:last_quiz_w12/data/mock_grocery_repository.dart';
+
+import '../../models/grocery.dart';
+
+class NewItem extends StatefulWidget {
+  const NewItem({super.key});
+
+  @override
+  State<NewItem> createState() {
+    return _NewItemState();
+  }
+}
+
+class _NewItemState extends State<NewItem> {
+  final _newForm = GlobalKey<FormState>();
+
+  // Default settings
+  static const defautName = "New grocery";
+  static const defaultQuantity = 1;
+  static const defaultCategory = GroceryCategory.fruit;
+
+  // Inputs
+  final _nameController = TextEditingController();
+  final _quantityController = TextEditingController();
+  GroceryCategory _selectedCategory = defaultCategory;
+
+  @override
+  void initState() {
+    super.initState();
+
+    // Initialize intputs with default settings
+    _nameController.text = defautName;
+    _quantityController.text = defaultQuantity.toString();
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+
+    // Dispose the controlers
+    _nameController.dispose();
+    _quantityController.dispose();
+  }
+
+  void onReset() {
+    // Will be implemented later - Reset all fields to the initial values
+    setState(() {
+      _nameController.text = defautName;
+      _quantityController.text = defaultQuantity.toString();
+      _selectedCategory = defaultCategory;
+    });
+  }
+
+  String _generateId() {
+    return String.fromCharCode(97 + dummyGroceryItems.length);
+  }
+
+  void onAdd() {
+    // Will be implemented later - Create and return the new grocery
+    if (_newForm.currentState!.validate()) {
+      final newGrocery = Grocery(
+        id: _generateId(),
+        name: _nameController.text,
+        quantity: int.parse(_quantityController.text),
+        category: _selectedCategory,
+      );
+
+      Navigator.of(context).pop(newGrocery);
+    }
+  }
+
+  String? validateName(String? value) {
+    if (value == null) {
+      return "The name should be filled";
+    }
+
+    if (value.isEmpty || value.length > 50) {
+      return "Enter a name from 1 to 50";
+    }
+
+    return null;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Add a new item')),
+      body: Padding(
+        padding: const EdgeInsets.all(12),
+        child: Form(
+          child: Column(
+            children: [
+              TextFormField(
+                // validator: validateName,
+                controller: _nameController,
+                maxLength: 50,
+                // onTap: validateName,
+                validator: validateName,
+                decoration: const InputDecoration(label: Text('Name')),
+              ),
+              const SizedBox(height: 10),
+              Row(
+                crossAxisAlignment: CrossAxisAlignment.end,
+                children: [
+                  Expanded(
+                    child: TextField(
+                      controller: _quantityController,
+                      decoration: const InputDecoration(
+                        label: Text('Quantity'),
+                      ),
+                    ),
+                  ),
+                  const SizedBox(width: 8),
+                  Expanded(
+                    child: DropdownButtonFormField<GroceryCategory>(
+                      initialValue: _selectedCategory,
+                      items: GroceryCategory.values.map((categoryItem) {
+                        return DropdownMenuItem(
+                          value: categoryItem,
+                          child: Row(
+                            children: [
+                              Container(
+                                color: categoryItem.color,
+                                width: 15,
+                                height: 15,
+                              ),
+                              const SizedBox(width: 8),
+                              Text(categoryItem.label),
+                            ],
+                          ),
+                        );
+                      }).toList(),
+                      onChanged: (value) {
+                        if (value != null) {
+                          setState(() {
+                            _selectedCategory = value;
+                          });
+                        }
+                      },
+                    ),
+                  ),
+                ],
+              ),
+              const SizedBox(height: 12),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: [
+                  TextButton(onPressed: onReset, child: const Text('Reset')),
+                  ElevatedButton(
+                    onPressed: onAdd,
+                    child: const Text('Add Item'),
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
